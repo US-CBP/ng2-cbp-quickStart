b1d46c2a28a983438241ee8c8749660c8f19b99b
martin-the-geek <martin2great@yahoo.com>
2016-10-07 18:53:33 -0400

    Add logic for defaultNode in dropdown tree

--- src/cf/dropdown-tree-field/dropdown-tree-field.component.ts
+++ src/cf/dropdown-tree-field/dropdown-tree-field.component.ts
@@ -5,8 +5,7 @@
     OnChanges,
     OnDestroy,
     OnInit,
-    Output,
-    SimpleChanges
+    Output
 }                               from "@angular/core";
 import { Subscription }         from "rxjs";
 
@@ -26,13 +25,16 @@ let nextId = 1;
 export class DropdownTreeFieldComponent implements OnInit, OnChanges, OnDestroy {
     @Input() id: string = createUniqueId();
     @Input() label: string;
+    @Input() defaultLabel: string;
     @Input() showFullSelectedPath: boolean = false;
     @Input() selectedNode: TreeNode;
     @Input() nodes: TreeNode[];
     @Output() nodeSelected = new EventEmitter<TreeNode>();
 
-    isDropdownExpanded: boolean = false;
+    isDropdownOpen: boolean = false;
     containerClasses: string[];
+    defaultNode: TreeNode;
+    selectedText: string;
 
     private stateSubscription: Subscription;
 
@@ -40,16 +42,25 @@ export class DropdownTreeFieldComponent implements OnInit, OnChanges, OnDestroy 
     }
 
     ngOnInit() {
+        this.defaultNode = this.initializeDefaultNode();
+
         let expandedNodes = new Set<TreeNode>();
         if(this.selectedNode != null) {
             this.addExpandedNodesToNodeFromArray(this.selectedNode, this.nodes, expandedNodes);
+            this.service.setState(null, this.selectedNode, expandedNodes);
+        } else {
+            this.service.setState(null, this.defaultNode, expandedNodes);
         }
 
-        this.service.setState(null, this.selectedNode, expandedNodes);
         this.stateSubscription = this.service.stateObservable.subscribe(this.onStateChange.bind(this));
     }
 
-    ngOnChanges(changes: SimpleChanges) {
+    ngOnChanges(changes: any) {
+        if(changes.defaultLabel || changes.selectedNode) {
+            this.defaultNode = this.initializeDefaultNode();
+        }
+
+        this.service.selectNode(this.selectedNode == null ? this.defaultNode : this.selectedNode);
     }
 
     ngOnDestroy() {
@@ -59,8 +70,8 @@ export class DropdownTreeFieldComponent implements OnInit, OnChanges, OnDestroy 
     }
 
     private onStateChange(state: DropdownTreeState) {
-        if(this.selectedNode !== state.selectedNode && !(this.selectedNode == null && state.selectedNode == null)) {
-            this.nodeSelected.next(state.selectedNode);
+        if(this.selectedNode !== state.selectedNode && !(this.selectedNode == null && state.selectedNode === this.defaultNode)) {
+            this.nodeSelected.next(state.selectedNode === this.defaultNode ? null : state.selectedNode);
         }
     }
 
@@ -87,6 +98,24 @@ export class DropdownTreeFieldComponent implements OnInit, OnChanges, OnDestroy 
         }
 
         return found;
+    }
+
+    private initializeDefaultNode() {
+        if(this.defaultLabel != null) {
+            return this.createDefaultNode(this.defaultLabel);
+        } else if(this.selectedNode == null) {
+            return this.createDefaultNode("");
+        } else {
+            return null;
+        }
+    }
+
+    private createDefaultNode(text: string): TreeNode {
+        return {
+            id: "-default-node",
+            text: text,
+            children: []
+        };
     }
 }
 
