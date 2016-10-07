0360c38f8114a92806967bed01565e4d0621fc78
martin-the-geek <martin2great@yahoo.com>
2016-10-07 15:25:54 -0400

    Add some tests for dropdown-tree-field component and reorder barrels

---
+++ src/cf/dropdown-tree-field/index.ts
@@ -0,0 +1,6 @@
+﻿export * from "./dropdown-tree-state.model";
+export * from "./tree-node.model";
+
+export * from "./dropdown-tree.service";
+export * from "./dropdown-tree-item";
+export * from "./dropdown-tree-field.component";
