cff1437f21bfe2711f93934de40d5f3df18de79b
bcz441c <martin.v.francisco@cbp.dhs.gov>
2016-10-06 21:11:40 -0400

    Add initial dropdown tree component

---
+++ src/cf/dropdown-tree-field/dropdown-tree-field.component.ts
@@ -0,0 +1,45 @@
+﻿import {
+    Component,
+    EventEmitter,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output
+}                               from "@angular/core";
+import { Subscription }         from "rxjs";
+
+import { DropdownTreeService }  from "./dropdown-tree.service";
+import { DropdownTreeState }    from "./dropdown-tree-state.model";
+import { TreeNode }             from "./tree-node.model";
+
+@Component({
+    selector: "cf-dropdown-tree-field",
+    templateUrl: "dropdown-tree-field.component.html",
+    providers: [
+        DropdownTreeService
+    ]
+})
+export class DropdownTreeFieldComponent implements OnInit, OnDestroy {
+    @Input() id: string;
+    @Input() label: string;
+    @Input() showFullSelectedPath: boolean;
+    @Input() nodes: TreeNode[];
+
+    private stateSubscription: Subscription;
+
+    constructor(private service: DropdownTreeService) {
+    }
+
+    ngOnInit() {
+        this.stateSubscription = this.service.stateObservable.subscribe(this.onStateChange.bind(this));
+    }
+
+    ngOnDestroy() {
+        if(this.stateSubscription != null) {
+            this.stateSubscription.unsubscribe();
+        }
+    }
+
+    private onStateChange(state: DropdownTreeState) {
+    }
+}
---
+++ src/cf/dropdown-tree-field/tree-node.model.ts
@@ -0,0 +1,5 @@
+﻿export interface TreeNode {
+    id: any;
+    text: string;
+    children: TreeNode[];
+}
