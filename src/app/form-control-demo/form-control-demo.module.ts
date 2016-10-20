c1d709d2a1f9cc24dbb7e6cc62e4d3a5b0f6aa1a
bcz441c <martin.v.francisco@cbp.dhs.gov>
2016-10-19 21:06:16 -0400

    Create initial demo for buttons

--- src/app/form-control-demo/form-control-demo.module.ts
+++ src/app/form-control-demo/form-control-demo.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule }                from "@angular/platform-browser";
 import { FormsModule }                  from "@angular/forms";
 
 import { CommonFrameworkModule }        from "ng2-cbp-cf/ng2-cbp-cf";
+import { ButtonDemoComponent }          from "./button-demo";
 import { DropdownTreeDemoComponent }    from "./dropdown-tree-demo";
 import { RadioButtonDemoComponent }     from "./radio-button-demo";
 import { formControlDemoRouting }       from "./form-control-demo.routing";
@@ -18,6 +19,7 @@ import { formControlDemoRouting }       from "./form-control-demo.routing";
         formControlDemoRouting
     ],
     declarations: [
+        ButtonDemoComponent,
         DropdownTreeDemoComponent,
         RadioButtonDemoComponent
     ],
--- src/app/form-control-demo/form-control-demo.routing.ts
+++ src/app/form-control-demo/form-control-demo.routing.ts
@@ -1,10 +1,12 @@
 import { ModuleWithProviders }  from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { ButtonDemoComponent } from "./button-demo";
 import { DropdownTreeDemoComponent } from "./dropdown-tree-demo";
 import { RadioButtonDemoComponent } from "./radio-button-demo";
 
 const formControlDemoRoutes: Routes = [
+     { path: "form-control-demo/button-demo", component: ButtonDemoComponent },
      { path: "form-control-demo/dropdown-tree-demo", component: DropdownTreeDemoComponent },
      { path: "form-control-demo/radio-button-demo", component: RadioButtonDemoComponent }
 ];
