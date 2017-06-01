0fb25997bc27d1f57685e0d882c23b1ce2677413
Ryan Kim <jk3kx.kim@gmail.com>
2017-06-01 09:39:08 -0400

    update styling

--- src/app/sample/user-management/user-management.component.ts
+++ src/app/sample/user-management/user-management.component.ts
@@ -11,6 +11,7 @@ import { User }                 from './user.model';
 
 @Component({
     templateUrl: 'user-management.component.html',
+    styleUrls: ['user-management.component.scss'],
 })
 export class UserManagementComponent implements OnInit {
     firstName: string;
