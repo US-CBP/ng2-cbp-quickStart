7185363130fbb89b67f64bd132191b00ee46d428
Ryan Kim <jk3kx.kim@gmail.com>
2017-06-14 14:59:16 -0400

    add toolbar to icons page

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+}                           from '@angular/core';
+import { ToolbarService }   from 'ng2-cbp-cf';
 
 @Component({
     templateUrl: 'icons.component.html',
     styleUrls: ['icons.component.scss'],
 })
 
-export class IconsComponent {
+export class IconsComponent implements OnInit {
+    constructor(private _toolbarService: ToolbarService) {
+    }
 
+    ngOnInit(): void {
+        this._toolbarService.setTitle('Material Design Icons - Guidelines');
+    }
     public iconList: any[] = [
         { icon: 'menu', name: 'Menu', description: 'Open and Close side nav' },
         { icon: 'person', name: 'User', description: 'User Accoun' },
@@ -30,7 +39,4 @@ export class IconsComponent {
         { icon: 'attach_file', name: 'Attachment', description: 'Uploading a document' },
         { icon: 'file_download', name: 'Download', description: 'Open document' },
     ];
-
-    constructor() {
-    }
 }
